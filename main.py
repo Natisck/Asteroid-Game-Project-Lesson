79f35ed96eb8a8f570e3c517cae87cf7676f0d3c
AlexPenq <alexpenguinqq@gmail.com>
2025-10-06 20:13:02 +0300

    B: added player + groups

--- main.py
+++ main.py
@@ -3,18 +3,36 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
+
+
 
 def main():
     print("Starting Asteroids!")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Player(SCREEN_WIDTH /2, SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        black    = (  0,   0,   0)
-        screen.fill(black)
-        pygame.display.flip ()
+        dt = clock.tick(60) / 1000
+        updatable.update(dt)
+
+        screen.fill("black")
+        for drawitem in drawable:
+            drawitem.draw(screen)
+
+        pygame.display.flip()
+
+
+
+    
 
 
 
